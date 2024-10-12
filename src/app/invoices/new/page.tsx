185f6be20b54d007c6ca1a4df1d13e2476e6e080
Dzhani <dzhani.ivanov@gmail.com>
2024-10-12 18:00:09 +0300

    add new invoice page

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,46 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function NewInvoice() {
+  return (
+    <main className="flex flex-col h-full justify-center  max-w-5l gap-6 mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold">Create Invoice</h1>
+      </div>
+      <form className="grid gap-4 max-w-xs">
+        <div>
+          <Label htmlFor="name" className="block mb-2 font-semibold text-sm">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="email" className="block mb-2 font-semibold text-sm">
+            Billing Email
+          </Label>
+          <Input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <Label htmlFor="value" className="block mb-2 font-semibold text-sm">
+            Value
+          </Label>
+          <Input name="value" id="value" type="text" />
+        </div>
+        <div>
+          <Label
+            htmlFor="description"
+            className="block mb-2 font-semibold text-sm"
+          >
+            Description
+          </Label>
+          <Textarea id="description" name="description" />
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
