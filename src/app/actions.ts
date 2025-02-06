9ed18b2fbce7900c7e8df11fa152cf23af3c52af
Dzhani <dzhani.ivanov@gmail.com>
2025-02-06 14:25:47 +0200

    ...

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,14 +2,21 @@
 
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
+import { redirect } from "next/navigation";
 
 export async function createAction(formData: FormData) {
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
 
-  await db.insert(Invoices).values({
-    value,
-    description,
-    status: "open",
-  });
+  const results = await db
+    .insert(Invoices)
+    .values({
+      value,
+      description,
+      status: "open",
+    })
+    .returning({
+      id: Invoices.id,
+    });
+  redirect(`/invoices/${results[0].id}`);
 }
--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,20 +1,36 @@
-import { sql } from "drizzle-orm";
-import { db } from "@/db";
-
+"use client";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
 import { createAction } from "@/app/actions";
+import { startTransition, SyntheticEvent, useState } from "react";
+import SubmitButton from "@/components/SubmitButton";
 
-export default async function NewInvoice() {
+export default function NewInvoice() {
+  const [state, setState] = useState("ready");
+
+  async function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+    if (state === "pending") return;
+    setState("pending");
+    const target = e.target as HTMLFormElement;
+    startTransition(async () => {
+      const formData = new FormData(target);
+      await createAction(formData);
+      console.log("test");
+    });
+  }
   return (
     <main className="flex flex-col h-full justify-center  max-w-5l gap-6 mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Create Invoice</h1>
       </div>
-      <form action={createAction} className="grid gap-4 max-w-xs">
+      <form
+        action={createAction}
+        onSubmit={handleSubmit}
+        className="grid gap-4 max-w-xs"
+      >
         <div>
           <Label htmlFor="name" className="block mb-2 font-semibold text-sm">
             Billing Name
@@ -43,7 +59,7 @@ export default async function NewInvoice() {
           <Textarea id="description" name="description" />
         </div>
         <div>
-          <Button className="w-full font-semibold">Submit</Button>
+          <SubmitButton />
         </div>
       </form>
     </main>
